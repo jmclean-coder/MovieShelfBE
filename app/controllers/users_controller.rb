44162ffafaff70e025d4b7e9b0dcccc821df48cf
Colton <kaiserc2@seattleu.edu>
2020-07-07 11:32:56 -0700

    Creates controllers, adds jwt gem, modifies routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.ApplicationController
+    render json: @paintings
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
