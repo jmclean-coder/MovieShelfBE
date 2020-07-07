f549467ee2f01f9fc2fe4e5ab5207868742f32af
Joshua Mclean <joshmcleana@gmail.com>
2020-07-07 09:49:11 -0700

    add user and movie validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
     has_many :movie_shelves
     has_many :shelves, through: :movie_shelves
+    validates :imdb_id, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_one :shelf
   has_secure_password
+
+  validates :username, :password_digest presence: true
+  validates :username, uniqueness: true
 end
