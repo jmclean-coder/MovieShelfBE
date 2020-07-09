8be72fddf7ed799c3ea9aa35b01f71e2c6310110
Justin Langlinais <justinclanglinais@gmail.com>
2020-07-09 14:41:10 -0700

    movie params fixed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,6 +18,7 @@ class MoviesController < ApplicationController
     movie = Movie.new(movie_params)
     if movie.valid?
       movie.save
+      shelf_movie = MovieShelf.create(movie_id: movie.id, shelf_id: 1)
       render json: movie, except: [:created_at, :updated_at] #handle in serializer later
     else
       render json: {message: movie.errors.full_messages}
