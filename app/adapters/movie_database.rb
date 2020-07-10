80bffae982da24797d9f3d7bc815f717708b3e19
Colton <kaiserc2@seattleu.edu>
2020-07-10 14:56:28 -0700

    Adds to gitignore

--- app/adapters/movie_database.rb
+++ app/adapters/movie_database.rb
@@ -1,22 +1,14 @@
 require "json"
 require "rest-client"
+
+API_KEY = ENV['OMDB_api_key']
 class MovieDatabase
-    def self.search(search_term)
-        url = "https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=#{search_term}"
-        response = RestClient.get(url, headers={
-            "X-RapidAPI-Host" => "movie-database-imdb-alternative.p.rapidapi.com",
-            "X-RapidAPI-Key" => "61cd997641mshecc536a501fa685p1ce10djsn9b8b0710bd78"
-            })
-            response_body = response.body
-            json_response = JSON.parse(response_body)
-            return json_response
-    end
 
     def self.get_movie(movieID)
         url = "https://movie-database-imdb-alternative.p.rapidapi.com/?i=#{movieID}&r=json"
         response = RestClient.get(url, headers={
             "X-RapidAPI-Host" => "movie-database-imdb-alternative.p.rapidapi.com",
-            "X-RapidAPI-Key" => "61cd997641mshecc536a501fa685p1ce10djsn9b8b0710bd78"
+            "X-RapidAPI-Key" => "#{API_KEY}"
             })
             response_body = response.body
             json_response = JSON.parse(response_body)
