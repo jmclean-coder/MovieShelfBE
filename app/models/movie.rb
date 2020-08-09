28d228473f5c596551dbacd46660a23d6aa0b848
Joshua Mclean <joshmcleana@gmail.com>
2020-08-09 15:49:59 -0700

    Serialize ratings string into array

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,5 @@ class Movie < ApplicationRecord
     has_many :movie_shelves, dependent: :destroy
     has_many :shelves, through: :movie_shelves
     validates :imdb_id, uniqueness: true
+    serialize :ratings, Array
 end
