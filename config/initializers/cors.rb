dc346463567897bde77bb9ed1dac0a566336ac58
Joshua Mclean <joshmcleana@gmail.com>
2020-08-18 16:16:44 -0700

    Update CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,16 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3000'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins 'https://movieshelfsearcher.herokuapp.com/'
+
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
