8a8a4c87f58a239095ed90a3cd8bbaccabdcd7c1
Joshua Mclean <joshmcleana@gmail.com>
2020-08-24 15:35:13 -0700

    Updared Cors Policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,7 +14,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://movieshelfsearcher.herokuapp.com/'
+    origins 'https://movieshelfsearcher.herokuapp.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
